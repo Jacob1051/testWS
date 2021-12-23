bad0f7efc515fd37f6ef461a730148acdc85d660
Jonathan Jacob <stevenjacob906@gmail.com>
2021-12-23 18:35:10 +0200

    update main

--- src/main/java/com/example/demo/Controller1.java
+++ src/main/java/com/example/demo/Controller1.java
@@ -1,13 +1,12 @@
-package com.example.demo.spring1.controller;
+package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller1 {
-	
 	@GetMapping("/message1")
-	public String message1() {
-		return "Message 1";
+	public String messageController1() {
+		return "Message Controller 1";
 	}
 }
--- src/main/java/com/example/demo/Controller2.java
+++ src/main/java/com/example/demo/Controller2.java
@@ -1,13 +1,12 @@
-package com.example.demo.spring2.controller;
+package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
-	
+public class Controller2 {
 	@GetMapping("/message2")
-	public String message2() {
-		return "Message 2";
+	public String messageController2() {
+		return "Message Controller 2";
 	}
 }
